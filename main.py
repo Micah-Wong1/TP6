9e44cf0f3f8f84ac8c4221260a05578f8175d191
Micah-Wong1 <Wongm@educ.college-montreal.qc.ca>
2026-04-08 12:09:25 -0400

    hmm finally started a bit

--- main.py
+++ main.py
@@ -2,20 +2,114 @@ import arcade
 import random
 from game_state import GameSystem
 
-screen_width = 640
-screen_height = 480
-window_title = "Arcade?"
+"""
+Starting Template
+
+Once you have learned how to use classes, you can begin your program with this
+template.
+
+If Python and Arcade are installed, this example can be run from the command line with:
+python -m arcade.examples.starting_template
+"""
+
+WINDOW_WIDTH = 1280
+WINDOW_HEIGHT = 720
+WINDOW_TITLE = "Starting Template"
+
+
+class GameView(arcade.View):
+    """
+    Main application class.
+
+    NOTE: Go ahead and delete the methods you don't need.
+    If you do need a method, delete the 'pass' and replace it
+    with your own code. Don't leave 'pass' in this program.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self.background_color = arcade.color.AMAZON
+
+        # If you have sprite lists, you should create them here,
+        # and set them to None
+
+    def reset(self):
+        """Reset the game to the initial state."""
+        # Do changes need to restart the game here if you want to support that
+        pass
+
+    def on_draw(self):
+        """
+        Render the screen.
+        """
+
+        # This command should happen before we start drawing. It will clear
+        # the screen to the background color, and erase what we drew last frame.
+        self.clear()
+
+        # Call draw() on all your sprite lists below
+
+    def on_update(self, delta_time):
+        """
+        All the logic to move, and the game logic goes here.
+        Normally, you'll call update() on the sprite lists that
+        need it.
+        """
+        pass
+
+    def on_key_press(self, key, key_modifiers):
+        """
+        Called whenever a key on the keyboard is pressed.
+
+        For a full list of keys, see:
+        https://api.arcade.academy/en/latest/arcade.key.html
+        """
+        pass
+
+    def on_key_release(self, key, key_modifiers):
+        """
+        Called whenever the user lets off a previously pressed key.
+        """
+        pass
+
+    def on_mouse_motion(self, x, y, delta_x, delta_y):
+        """
+        Called whenever the mouse moves.
+        """
+        pass
+
+    def on_mouse_press(self, x, y, button, key_modifiers):
+        """
+        Called when the user presses a mouse button.
+        """
+        pass
+
+    def on_mouse_release(self, x, y, button, key_modifiers):
+        """
+        Called when a user releases a mouse button.
+        """
+        pass
 
 
 def main():
-    arcade.open_window(screen_width, screen_height, window_title)
-    arcade.set_background_color(arcade.color.MIDNIGHT_BLUE)
+    """ Main function """
+    # Create a window class. This is what actually shows up on screen
+    window = arcade.Window(WINDOW_WIDTH, WINDOW_HEIGHT, WINDOW_TITLE)
 
-    arcade.start_render()
+    # Create and setup the GameView
+    game = GameView()
 
-    arcade.finish_render()
+    # Show GameView on screen
+    window.show_view(game)
+
+    # Start the arcade game loop
     arcade.run()
 
 
-main()
+if __name__ == "__main__":
+    main()
 
+screen_width = 640
+screen_height = 480
+window_title = "Arcade?"
